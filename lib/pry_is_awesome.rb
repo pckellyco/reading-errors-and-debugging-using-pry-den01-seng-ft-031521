94d95acc79ed12e8839982d636d8585ddbfb36c2
Paige Kelly <pckelly.co@gmail.com>
2021-01-24 22:34:12 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -8,5 +8,3 @@ def prying_into_the_method
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
 end
-
-prying_into_the_method
